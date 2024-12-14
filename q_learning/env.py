95c234de9a49e327550493414259b4bc04790e2c
Duy-Nam Bui <duynam.robotics@gmail.com>
2024-12-14 21:35:18 +0700

    Q learning method

--- q_learning/env.py
+++ q_learning/env.py
@@ -4,61 +4,45 @@ import time
 class Env():
     def __init__(self):
         self.action_space = ['up', 'down', 'left', 'right']
-        self.n_actions = len(self.action_space)
-
-        # Show the steps for shortest route
-        self.shortest_path = []
+        self.action_map = {'up': (-1, 0), 'down': (1, 0), 'left': (0, -1), 'right': (0, 1)}
+        self.create_environment()
         self.reset()
 
+    def create_environment(self):
+        ''' Environment configuration
+        '''
+        self.rows = 5
+        self.cols = 5
+        self.goal = (0, 4)
+        self.obstacles = [(2, 2), (3, 3), (0, 3), (1, 3), (2, 3)]
+
     def reset(self):
-        # Create map with obstacles
-        self.x_dim = 10     # X dimension
-        self.y_dim = 10     # Y dimension 
-        self.env = np.zeros([self.y_dim, self.x_dim])   # 0 - free, 1 - obstacle
-        obs = (np.array([1,1,1,3,4,5,0,1,2,3,4,5,6,7,8,1,1]),
-               np.array([1,2,3,3,3,3,6,6,6,6,6,6,6,6,6,8,9]))
-        self.env[obs] = 1
-
-        # Final target
-        self.goal = np.array([0,9])
-
-        # Current agent
-        self.agent = np.array([0,0])
-
-        # Save path
-        self.path = [self.agent]
+        ''' Reset environment and agent
+        '''
+        self.agent = (0, 0)
+        
+    def is_valid(self, state):
+        ''' Check valid state
+        '''
+        r, c = state
+        return 0 <= r < self.rows and 0 <= c < self.cols and state not in self.obstacles
 
     def step(self, action):
-        # Moving agent
-        if action == 0 and self.agent[1] >= 1:                  # up
-            self.agent[1] = self.agent[1] - 1
-        elif action == 1 and self.agent[1] <= self.x_dim - 2:   # down
-            self.agent[1] = self.agent[1] + 1
-        elif action == 2 and self.agent[0] >= 1:                # left
-            self.agent[0] = self.agent[0] - 1
-        elif action == 3 and self.agent[0] <= self.y_dim - 2:   # right
-            self.agent[0] = self.agent[0] + 1
+        ''' Compute the reward funtion and update agent's state
+        '''
+        r, c = self.agent
+        dr, dc = self.action_map[action]
+        next_state = (r + dr, c + dc)
 
-        # Store the path
-        self.path.append(self.agent)
+        if self.is_valid(next_state):
+            self.agent = next_state # Update state
 
         # Compute the reward
-        if self.agent[0] == self.goal[0] and self.agent[1] == self.goal[1]: # Reach target
-            reward = 1
-            done = True
-        elif self.env[self.agent[0], self.agent[1]] == 1: # Collision
-            reward = -1
-            done = True
+        if self.agent == self.goal:
+            return 100
+        elif self.agent in self.obstacles:
+            return -100
         else:
-            reward = 0
-            done = False
-
-        return reward, done
-
-    def render(self):
-        self.update()
-
-    def final(self):
-        ...
+            return -1
 
     
--- q_learning/q_learning.py
+++ q_learning/q_learning.py
@@ -1,2 +1,65 @@
 import numpy as np
-import pandas as pd
+import random
+
+from env import Env
+
+class QLearning:
+    def __init__(self, env:Env, learning_rate=0.01, reward_decay=0.9, explore_rate=0.9):
+        self.env = env
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon = explore_rate
+
+        # Initialize Q-table
+        self.Q_table = np.zeros((self.env.rows,
+                                 self.env.cols,
+                                 len(self.env.action_space)))
+
+    def choose_action(self, state):
+        ''' Choose action (epsilon-greedy)
+        '''
+        if random.uniform(0, 1) < self.epsilon:
+            return random.choice(self.env.action_space)  # Explore
+        else:
+            row, col = state
+            return self.env.action_space[np.argmax(self.Q_table[row, col])]  # Exploit
+        
+    def learn(self, num_episodes=1000):
+        ''' Train Q-learning agent
+        '''
+        for _ in range(num_episodes):
+            self.env.reset()
+            while self.env.agent != self.env.goal:
+                r, c = self.env.agent
+                action = self.choose_action(self.env.agent)
+                reward = self.env.step(action)
+                nr, nc = self.env.agent
+
+                # Q-learning update
+                self.Q_table[r, c, self.env.action_space.index(action)] += self.lr * (
+                    reward + \
+                    self.gamma * np.max(self.Q_table[nr, nc]) - \
+                    self.Q_table[r, c, self.env.action_space.index(action)]
+                )
+    
+    def execution(self, state):
+        # Extract optimal path
+        path = [state]
+        while state != self.env.goal:
+            r, c = state
+            action = self.env.action_space[np.argmax(self.Q_table[r, c])]
+            dr, dc = self.env.action_map[action]
+            state = (r + dr, c + dc)
+            path.append(state)
+        return path
+
+if __name__ == "__main__":
+    # Learning process
+    env = Env()
+    planner = QLearning(env)
+    planner.learn(num_episodes=1000)
+
+    # Execution
+    state = (1, 2)
+    path = planner.execution(state)
+    print("Optimal Path:", path)
